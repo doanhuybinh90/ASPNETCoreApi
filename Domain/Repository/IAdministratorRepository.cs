89565b269c0dde8546c794fc949f989f0bf03cab
Larissa Heloisa Rodrigues do Nascimento <LarissaRN@grupotba.intra.corp>
2020-01-28 13:32:38 -0300

    Created Repository in Domain

---
+++ Domain/Repository/IAdministratorRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repository
+{
+    public interface IAdministratorRepository : IRepository<Administrator>
+    {
+        Task<Administrator> FindByLogin(string email);
+    }
+}
---
+++ Domain/Repository/IVisitorRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repository
+{
+    public interface IVisitorRepository : IRepository<Visitor>
+    {
+        Task<Visitor> FindByLogin(string email);
+    }
+}
